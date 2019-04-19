958dda7b0bbc0f7d1f5eb5f2fdbe1767a9e9901f
Steve Ferenchak <steveferenchak@gmail.com>
2019-04-19 10:32:58 -0700

    Updates

---
+++ precache-manifest.ae28e333189f63338673e51f5d8605c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad827d23e73279ca75eb",
+    "url": "/react-videos/static/js/runtime~main.f892f45d.js"
+  },
+  {
+    "revision": "af95068e3a3d22f3bf69",
+    "url": "/react-videos/static/js/main.f63c5e5a.chunk.js"
+  },
+  {
+    "revision": "6b7a8549d14891e00d91",
+    "url": "/react-videos/static/js/2.1e7b24b2.chunk.js"
+  },
+  {
+    "revision": "af95068e3a3d22f3bf69",
+    "url": "/react-videos/static/css/main.997713c6.chunk.css"
+  },
+  {
+    "revision": "e23db8ab25bb94c6158465f13c4e83f0",
+    "url": "/react-videos/index.html"
+  }
+];
